33f6b0489a98ac8f7591cef742a288b880beedf0
ezgifistikcioglu <fistikciogluezgi@gmail.com>
2024-10-11 18:21:15 +0300

    Update library versions and configure project with Hilt, Retrofit, and Compose setup - Updated library versions in lib.versions.toml:   - Upgraded AGP, Coil,Gson Converter, Hilt, Kotlin and Included Material3 for modern UI components.
    - Configured top-level build.gradle.kts:
      - Added plugins using alias for android-application, kotlin-android, and hilt.
      - Defined a custom clean task to clear build directories.
    
    - Configured app-level build.gradle.kts:
      - Enabled Compose features and set up Kotlin Compiler extension.
      - Added dependencies for Hilt, Retrofit, Gson, Coil, Coroutines, ViewModel, and testing libraries.
      - Set up Hilt integration for DI and Retrofit for API calls.
      - Integrated Coil for image loading and Material3 for enhanced UI components.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.hilt.android)
+    id("kotlin-kapt")
 }
-
 android {
     namespace = "com.ezgieren.weatherapp"
     compileSdk = 34
@@ -15,55 +16,60 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3"
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            proguardFiles (getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 }
 
 dependencies {
+    implementation (libs.androidx.core.ktx)
+    implementation (libs.androidx.lifecycle.runtime.ktx)
+    implementation (libs.androidx.activity.compose)
+    implementation (libs.ui)
+    implementation (libs.ui.tooling.preview)
+    implementation (libs.androidx.material)
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    // Hilt
+    implementation (libs.hilt.android)
+    implementation(libs.androidx.material3.android)
+    kapt (libs.hilt.compiler)
+
+    // Retrofit & Gson
+    implementation (libs.retrofit)
+    implementation (libs.converter.gson)
+
+    // Coroutine Support
+    implementation (libs.kotlinx.coroutines.core)
+    implementation (libs.kotlinx.coroutines.android)
+
+    // ViewModel & LiveData
+    implementation (libs.androidx.lifecycle.viewmodel.ktx)
+    implementation (libs.lifecycle.livedata.ktx)
+
+    // Coil (Image Loading)
+    implementation (libs.coil.compose)
+
+    // Testing Libraries
+    testImplementation (libs.junit)
+    androidTestImplementation (libs.androidx.junit)
+    androidTestImplementation (libs.androidx.espresso.core)
+    androidTestImplementation (libs.ui.test.junit4)
+    debugImplementation (libs.ui.tooling)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,9 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.hilt.android) apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(layout.buildDirectory)
 }
